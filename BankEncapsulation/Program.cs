2f61ea39e9b608ad0c3c419f203b63a915b200ff
Amir Gaffar <AmirG7@yahoo.com>
2020-11-03 12:47:55 -0500

    Project Completed

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var amirsaccount = new BankAccount();
+            Console.WriteLine("How much would you like to deposit?");
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            amirsaccount.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank you! Your balance is now {amirsaccount.GetBalance()}");
+        }
+    }
+}
